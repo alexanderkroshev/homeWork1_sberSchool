277e830d2f6267c2a6a1d56ec3d9d7e010e7e9f9
alexanderkroshev <alexanderkroshev@gmail.com>
2021-12-22 12:59:07 +0700

    Initial commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    }
+}
